39ba18ead93d2b7897cd59362940d08fb8df2278
Znomon <kaseyfkolyno@gmail.com>
2016-02-19 22:57:37 -0500

    organized into definitions

--- Messenger.py
+++ Messenger.py
@@ -4,10 +4,12 @@
 
 import socket
 
-TCP_PORT = '127.0.0.1' #for testing purposes
-TCP_PORT = 9999        #personal preferance 
+def init_connection ():
+	TCP_IP = '127.0.0.1'   #for testing purposes (0.0.0.0 for INET)
+	TCP_PORT = 9999        #personal preferance 
+	BUFFER = 256
+	skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the skt variable as a IP/TCP port
+	skt.connect((TCP_IP, TCP_PORT))							#binds to socket/IP
 
-BUFFER = 2048
-
-skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the skt variable as a IP/TCP port
-skt.connect((TCP_IP, TCP_PORT))							#binds to socket/IP
+def send_msg(text):	#define a send_msg function
+	s.send(text)
