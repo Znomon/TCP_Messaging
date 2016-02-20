6702feb4c59a3d35b113f9948bdf83b530403af4
Znomon <kaseyfkolyno@gmail.com>
2016-02-20 00:52:19 -0500

    Successfully modularized receiving messages

--- Messenger.py
+++ Messenger.py
@@ -4,12 +4,25 @@
 
 import socket
 
-def init_connection ():
-	TCP_IP = '127.0.0.1'   #for testing purposes (0.0.0.0 for INET)
-	TCP_PORT = 9999        #personal preferance 
-	BUFFER = 256
-	skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the skt variable as a IP/TCP port
-	skt.connect((TCP_IP, TCP_PORT))							#binds to socket/IP
+class msg():
+   def init_connection(self):
+      TCP_IP = '127.0.0.1'   #for testing purposes (0.0.0.0 for INET)
+      TCP_PORT = 9999        #personal preferance 
+      self.skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the skt variable as a IP/TCP port
+      self.skt.bind((TCP_IP, TCP_PORT))                            #binds to socket/IP
+      self.skt.listen(1)                                           #listens on that port
 
-def send_msg(text):	#define a send_msg function
-	s.send(text)
+   def send_msg(text):	#define a send_msg function
+      self.skt.send(text)
+
+   def rec_msg(self):
+      BUFFER = 256
+      conn, addr = self.skt.accept()
+      data = conn.recv(BUFFER)
+      print data
+
+ping = msg()
+ping.init_connection()
+ping.rec_msg()
+   
+   
